fcf1dbdb114e269f74fa5785280d9070facb7470
ImadEddineBek <bek34046629@yahoo.com>
2017-11-10 14:31:10 +0100

    Shit is about to go down

--- Competition/__init__.py
+++ Competition/__init__.py
@@ -1,187 +1,191 @@
-import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-import numpy as np
 import tensorflow as tf
+import numpy as np
 import pandas as pd
-import math
+from sklearn import preprocessing
+if __name__ == '__main__':
+    def to_onehot(labels, nclasses=3):
+        '''
+        Convert labels to "one-hot" format.
 
-def to_onehot(labels, nclasses=3):
-    outlabels = np.zeros((len(labels), nclasses))
-    for i, l in enumerate(labels):
-        outlabels[i, l] = 1
-    return outlabels
+        >>> a = [0,1,2,3]
+        >>> to_onehot(a,5)
+        array([[ 1.,  0.,  0.,  0.,  0.],
+               [ 0.,  1.,  0.,  0.,  0.],
+               [ 0.,  0.,  1.,  0.,  0.],
+               [ 0.,  0.,  0.,  1.,  0.]])
+        '''
+        outlabels = np.zeros((len(labels), nclasses))
+        for i, l in enumerate(labels):
+            outlabels[i, l] = 1
+        return outlabels
+    def predictOnTest(a: int):
+        pred = sess.run(y, feed_dict={x: testing_data})
 
+        sub = pd.read_csv('sample_submission.csv')
+        sub['target'] = pred
+        sub['target'] = sub['target'].astype(int)
+        sub.to_csv('deepNeural' + str(a) + '.csv', index=False)
 
-f = 4
-
-
-def next(f):
-    f += 1
-    return np.exp(-f) / (1 + np.exp(-f)), f
-
-
-train = pd.read_csv('train_data.csv')
-
-feature_names = [x for x in train.columns if x not in ['connection_id', 'target']]
-target = train['target'].values
-train = train[feature_names].values
-onehot = to_onehot(target)
-
-indices = np.random.permutation(train.shape[0])
-valid_cnt = int(train.shape[0] * 0.1)
-
-test_idx, training_idx = indices[:valid_cnt], indices[valid_cnt:]
-test, train = train[test_idx, :], train[training_idx, :]
-onehot_test, onehot_train = onehot[test_idx, :], onehot[training_idx, :]
-
-x = tf.placeholder(tf.float32, shape=(None, 41))  # shape (plusieurs,'''41''')
-y_ = tf.placeholder(tf.float32, shape=(None, 3), name='y_')
-
-
-num_hidden = 128
-num_hidden2 = 256
-num_hidden3 = 128
-W1 = tf.Variable(tf.truncated_normal([41, num_hidden],
-                                     stddev=1. / math.sqrt(41)))
-b1 = tf.Variable(tf.constant(0.1, shape=[num_hidden]))
-h1 = tf.sigmoid(tf.matmul(x, W1) + b1)
+    def DoIpredictOnTest(i: int):
+        file = open('DoIWrite', mode='r')
+        rep = file.read()
+        if rep == 'yes':
+            predictOnTest(i)
+            file.close()
+            file = open('DoIWrite', mode='w')
+            file.write('')
+            file.close()
 
 
-
-W11 = tf.Variable(tf.truncated_normal([num_hidden, num_hidden2],
-                                     stddev=1. / math.sqrt(num_hidden)))
-b11 = tf.Variable(tf.constant(0.1, shape=[num_hidden2]))
-h11 = tf.sigmoid(tf.matmul(h1, W11) + b11)
-
-
-W12 = tf.Variable(tf.truncated_normal([num_hidden2, num_hidden3],
-                                     stddev=1. / math.sqrt(num_hidden2)))
-b12 = tf.Variable(tf.constant(0.1, shape=[num_hidden3]))
-h12 = tf.sigmoid(tf.matmul(h11, W12) + b12)
-
-num_hidden4 = 256
-
-W13 = tf.Variable(tf.truncated_normal([num_hidden3, num_hidden4],
-                                     stddev=1. / math.sqrt(num_hidden3)))
-b13 = tf.Variable(tf.constant(0.1, shape=[num_hidden4]))
-h13 = tf.sigmoid(tf.matmul(h12, W13) + b13)
-
-# Output Layer
-W2 = tf.Variable(tf.truncated_normal([num_hidden3, 3],
-                                     stddev=1. / math.sqrt(3)))
-b2 = tf.Variable(tf.constant(0.1, shape=[3]))
-y = tf.nn.softmax(tf.matmul(h12, W2) + b2)
+    def stop(i: int):
+        file = open('DoIWrite', mode='w')
+        file.write('yes')
+        file.close()
+        DoIpredictOnTest(i)
+        file = open('DoIStop', mode='r')
+        rep = file.read()
+        if rep == 'yes':
+            i = -10
+            file.close()
+            file = open('DoIStop', mode='w')
+            file.write('')
+            file.close()
+        return i
 
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+    train = pd.read_csv('train_data.csv')
+    testing_data = pd.read_csv('test_data.csv')
 
-train_step = tf.train.AdamOptimizer(0.1).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
+    # extracting features and targets
+    feature_names = [x for x in train.columns if x not in ['connection_id', 'target']]
+    target = train['target']
+    train = train[feature_names]
+    testing_data = testing_data[feature_names]
+    # normalizing
+    min_max_scaler = preprocessing.StandardScaler()
+    train = min_max_scaler.fit_transform(train)
+    testing_data = min_max_scaler.fit_transform(testing_data)
 
 
 
-maxTest = 0
-maxTrain = 0
-maxI = 0
-maxJ = 0
-stuck = 0
-learning_rate = 0.0001
+    onehot = to_onehot(target)
+
+    # Split data into training and validation
+    indices = np.random.permutation(train.shape[0])
+    valid_cnt = int(train.shape[0] * 0.02)
+    dev_idx, training_idx = indices[:valid_cnt], indices[valid_cnt:]
+    dev, train = train[dev_idx, :], train[training_idx, :]
+    onehot_dev, onehot_train = onehot[dev_idx, :], onehot[training_idx, :]
+
+    sess = tf.InteractiveSession()
+
+    # These will be inputs
+    ## Input pixels, flattened
+    x = tf.placeholder("float", [None, 41])
+    ## Known labels
+    y_ = tf.placeholder("float", [None, 3])
 
 
-def maximize(learning_rate):
-    if learning_rate >= 1 :
-        return 1
-    return learning_rate * 10
+    def hiddenLayer(previous_layer, num_previous_hidden, num_hidden: int):
+        W = tf.Variable(tf.truncated_normal([num_previous_hidden, num_hidden],
+                                            stddev=2. / np.math.sqrt(num_previous_hidden)))
+        b = tf.Variable(tf.constant(0.1, shape=[num_hidden]))
+        h = tf.nn.relu(tf.matmul(previous_layer, W) + b)
+        return h
 
 
-def minimize(learning_rate):
-    if learning_rate <= 0.000001 :
-        return 0.000001
-    return learning_rate / 1.6
-i = 0
-v= 0
-while v <78.5 and i <2000:
-    sess.run(train_step, feed_dict={y_: onehot_train, x: train})
-
-    currentTest = sess.run(accuracy, feed_dict={y_: onehot_test, x: test})
-    currentTrain = sess.run(accuracy, feed_dict={y_: onehot_train, x: train})
-    v = currentTest
-    if currentTrain > maxTrain and currentTest > maxTest :
-        maxTrain = currentTrain
-        maxTest = currentTest
-        print(i, "better train", currentTrain)
-        maxJ = i
-        print(i, "better test", currentTest)
-        maxI = i
-        # learning_rate = minimize(learning_rate)
-        # train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
-        # stuck -=1
-        if currentTrain > 0.78 or currentTest > 0.78:
-            with open('log.txt', mode='a')as file:
-                print(b1.eval(), 'b1', file=file)
-                print(b11.eval(), 'b11', file=file)
-                print(b12.eval(), 'b12', file=file)
-                print(b2.eval(), 'b2', file=file)
-                print(W1.eval(), 'W1', file=file)
-                print(W12.eval(), 'W12', file=file)
-                print(W2.eval(), 'W2', file=file)
-                currentTest = sess.run(accuracy, feed_dict={y_: onehot_test, x: test})
-                currentTrain = sess.run(accuracy, feed_dict={y_: onehot_train, x: train})
-                print(currentTest, 'currentTest', file=file)
-                print(currentTrain, 'currentTrain', file=file)
-    #     W_Max = W
-    #     B_Max = B
-    stuck += 1
-    print(i)
-    i+=1
+    num_of_input = 41
+    num_of_hidden_units_in_layer_1 = 41
+    num_of_hidden_units_in_layer_2 = 41
+    num_of_hidden_units_in_layer_3 = 41
+    num_of_hidden_units_in_layer_4 = 41
+    num_of_hidden_units_in_layer_5 = 41
+    num_of_hidden_units_in_layer_6 = 41
+    num_of_hidden_units_in_layer_7 = 41
+    num_of_hidden_units_in_layer_8 = 41
+    num_of_output_classes = 3
+    # input layer
+    layer1 = hiddenLayer(x, num_of_input, num_of_hidden_units_in_layer_1)
+    # hidden layers
+    layer2 = hiddenLayer(layer1, num_of_hidden_units_in_layer_1, num_of_hidden_units_in_layer_2)
+    layer3 = hiddenLayer(layer2, num_of_hidden_units_in_layer_2, num_of_hidden_units_in_layer_3)
+    layer4 = hiddenLayer(layer3, num_of_hidden_units_in_layer_3, num_of_hidden_units_in_layer_4)
+    layer5 = hiddenLayer(layer4, num_of_hidden_units_in_layer_4, num_of_hidden_units_in_layer_5)
+    layer6 = hiddenLayer(layer5, num_of_hidden_units_in_layer_5, num_of_hidden_units_in_layer_6)
+    layer7 = hiddenLayer(layer6, num_of_hidden_units_in_layer_6, num_of_hidden_units_in_layer_7)
+    layer8 = hiddenLayer(layer7, num_of_hidden_units_in_layer_7, num_of_hidden_units_in_layer_8)
+    # output layer
+    W_last = tf.Variable(tf.truncated_normal([num_of_hidden_units_in_layer_8, num_of_output_classes],
+                                             stddev=2. / np.math.sqrt(num_of_output_classes)))
+    b_last = tf.Variable(tf.constant(0.1, shape=[num_of_output_classes]))
 
 
-print(sess.run(accuracy, feed_dict={y_: onehot_train, x: train}))
-print(sess.run(accuracy, feed_dict={y_: onehot_test, x: test}))
-print(maxJ,maxI,maxTest,maxTrain)
-testing_data = pd.read_csv('test_data.csv')
-testing_data = testing_data[feature_names].values
+    # Define model
+    y = tf.nn.softmax(tf.matmul(layer8, W_last) + b_last)
 
-m = sess.run(y, feed_dict={x: testing_data})
+    ### End model specification, begin training code
 
 
-def index(l):
-    j = 0
-    max = -1
-    for i in range(3):
-        if l[i] > max:
-            max = l[i]
-            j = i
-    return j
+    # Climb on cross-entropy
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y + 1e-50))
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    # How we train
+    train_step = tf.train.RMSPropOptimizer(0.01).minimize(cross_entropy)
+
+    train_acc_var = tf.Variable(0.0, dtype=tf.float32)
+    tf.summary.scalar('accuracy train', train_acc_var)
+    dev_acc_var = tf.Variable(0.0, dtype=tf.float32)
+    tf.summary.scalar('accuracy dev', dev_acc_var)
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter('learning/train',
+                                         sess.graph)
+    test_writer = tf.summary.FileWriter('learning/test')
 
 
-def back_to_normal(array):
-    list = np.zeros(len(array), dtype=np.int)
-    for i, l in enumerate(array):
-        list[i] = index(l)
-    return list
+    indices = np.random.permutation(train.shape[0])
+    i = 0
+    max_dev = 0
+    max_train = 0
+    max_dev_index = 0
+    max_train_index = 0
+    batch = 8296
+    tf.global_variables_initializer().run()
+    while i != -10:
+        train_acc = []
+        dev_acc = 0
+        first = 0
+        last = batch
+        for f in range(20):
+            sess.run(train_step, feed_dict={x: train[indices][first:last], y_: onehot_train[indices][first:last]})
+            train_acc.append(
+                sess.run(accuracy, feed_dict={x: train[indices][first:last], y_: onehot_train[indices][first:last]}))
+        summary, dev_acc = (sess.run([merged, accuracy], feed_dict={x: dev, y_: onehot_dev}))
+        test_writer.add_summary(summary, i)
+        train_acc = np.mean(train_acc)
+        train_acc_var = tf.Variable(train_acc,tf.float32)
+        sess.run(train_acc_var.initializer)
+        summary, train_acc_var = (sess.run([merged, train_acc_var]))
+        train_writer.add_summary(summary, i)
+        if train_acc > max_train:
+            max_train = train_acc
+            max_train_index = i
+            print("                                  better train", max_train, i)
+        if dev_acc > max_dev:
+            max_dev = dev_acc
+            max_dev_index = i
+            print("                                  better dev", max_dev, i)
+        print(i, train_acc, dev_acc)
+        i += 1
+        DoIpredictOnTest(i)
+        i = stop(i)
 
 
-sub = pd.read_csv('sample_submission.csv')
-sub['target'] = back_to_normal(m)
-sub['target'] = sub['target'].astype(int)
-sub.to_csv('sub1.csv', index=False)
+    def predictOnTest(a: int):
+        pred = sess.run(y, feed_dict={x: testing_data})
 
-
-
-
-
-
-
-
-# from sklearn.metrics import accuracy_score
-# def multAcc(pred, dtrain):
-#     label = dtrain.get_label()
-#     acc = accuracy_score(label, pred)
-#     return 'maccuracy', acc
+        sub = pd.read_csv('sample_submission.csv')
+        sub['target'] = pred
+        sub['target'] = sub['target'].astype(int)
+        sub.to_csv('deepNeural' + str(a) + '.csv', index=False)
--- machine_learning_systems/section_1/learning from mnist.py
+++ machine_learning_systems/section_1/learning from mnist.py
@@ -1,12 +1,120 @@
-n = int(input())
-s = map(int ,input().split())
-stack = []
-for num in s:
-    for i in range(2, num):
-        if (num % i) == 0:
-            stack.append(num)
-            break
-    if i >= num-1:
-        print(num,end=' ')
-print()
-print(*stack)
+# list = list(map(int, input().split()))
+# m = list[0]
+# n = list[1]
+# matrix = []
+#
+#
+# def arrr(param: str):
+#     arr = []
+#     for s in param:
+#         arr.append(s)
+#     return arr
+#
+#
+# for _ in range(m):
+#     matrix.append(arrr(input()))
+# perimeters = []
+# def rest(ma:list, kkkk, iiii):
+#     newMatrix = []
+#     for ii , l in enumerate(ma):
+#         if ii >= kkkk :
+#             ligne = []
+#             for j , v in enumerate(l):
+#                 if j >= iiii:
+#                     ligne.append(v)
+#             newMatrix.append(ligne)
+#     return newMatrix
+#
+#
+# def column(looking:list, param:int):
+#     newMatrix = []
+#     for ii, l in enumerate(looking):
+#         for j, v in enumerate(l):
+#             if j == param:
+#                 newMatrix.append(v)
+#     return newMatrix
+#
+#
+# def biggestPerimeter(matrix, k: int, i: int):
+#     looking = rest(matrix,k, i)
+#     for c in looking[0]:
+#         if c == 'x':
+#             return 0
+#     for c in column(looking,0):
+#         if c == 'x':
+#             return 0
+#     for c in looking[-1]:
+#         if c == 'x':
+#             return 0
+#     for c in column(looking,-1):
+#         if c == 'x':
+#             return 0
+#     return 2 * (len(looking) - 1) + 2 * (len(looking[0]) - 1)
+#
+# #
+# for k in range(m - 1):
+#     line = matrix[k]
+#     for i in range(n - 1):
+#         if line[i] == '.':
+#             perimeters.append(biggestPerimeter(matrix, k, i))
+# max = max(perimeters)
+# if max == 0:
+#     print('impossible')
+# else:
+#     print(max)
+#
+# import os
+#
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#
+# import numpy as np
+# import tensorflow as tf
+# import pandas as pd
+# import math
+#
+#
+# def to_onehot(labels, nclasses=3):
+#     outlabels = np.zeros((len(labels), nclasses))
+#     for i, l in enumerate(labels):
+#         outlabels[i, l] = 1
+#     return outlabels
+#
+#
+# f = 4
+#
+#
+# def next(f):
+#     f += 1
+#     return np.exp(-f) / (1 + np.exp(-f)), f
+#
+#
+# train = pd.read_csv('train_data.csv')
+
+def mediane(list: list):
+    if len(list) % 2 == 1:
+        return list[(len(list)) // 2]
+    else:
+        return (list[(len(list)) // 2] + list[(len(list)) // 2 - 1]) / 2
+
+length = int(input())
+list = list(map(int, input().split()))
+list.sort()
+n = 0
+upper = []
+lower = []
+if len(list) % 2 == 0:
+    n = len(list) // 2 - 1
+    lower = list[:len(list) // 2]
+    upper = list[len(list) // 2:]
+    q2 = (list[(len(list)) // 2] + list[(len(list)) // 2 - 1]) / 2
+else:
+    n = len(list) // 2
+    q2 = list[n]
+    lower = list[:len(list) // 2]
+    upper = list[len(list) // 2 + 1:]
+q1 = mediane(lower)
+q3 = mediane(upper)
+print(q1)
+print(q2)
+print(q3)
+
